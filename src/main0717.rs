86b522360d723227a6423f0ac4a8343c0456ccb8
MaLei <826908021@qq.com>
2019-07-18 16:22:15 +0800

    07-18更新

---
+++ src/main0717.rs
@@ -0,0 +1,58 @@
+// @author : MaLei 
+// @datetime : 2019-07-16 17:43
+// @file : main.py
+// @software : PyCharm
+
+
+//fn main() {
+//    let x = 2.0; // f64
+//    println!("{}",x);
+//
+//    let y: f32 = 3.0; // f32
+//    println!("{}",y);
+//}
+
+//fn main() {
+//    let sum = 5 + 10;   // 加法
+//    let difference = 95.5 - 4.3;    // 减法
+//    let product = 4 * 30;   // 乘法
+//    let quotient = 56.7 / 32.2;     // 除法
+//    let remainder = 43 % 5;     // 取余
+//}
+
+//fn main() {
+//    let t = true;
+//    let f: bool = false; // 显式指定类型注解
+//}
+
+//fn another_function(x: i32, y: i32) {
+//    println!("The value of x is: {}", x);
+//    println!("The value of y is: {}", y);
+//}
+fn five() -> i32 {
+    5
+}
+
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {}", x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
