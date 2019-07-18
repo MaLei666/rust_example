10b8c93fef7091ee23c8cdc4f154e066d1a94c9e
MaLei <826908021@qq.com>
2019-07-18 18:17:06 +0800

    07-18更新

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,48 @@
 // @datetime : 2019-07-18 16:21
 // @file : main.py
 // @software : PyCharm
-fn main() {}
+//fn main() {
+//    let condition = true;
+//
+//    let number = if condition {
+//        5
+//    } else {
+//        6
+//    };
+//
+//    println!("The value of number is: {}", number);
+//}
+//
+//fn main() {
+//    let mut counter = 0;
+//
+//    let result = loop {
+//        counter += 1;
+//        println!("num:{}",counter);
+//        if counter == 10 {
+//            break counter * 2;
+//        }
+//    };
+//
+//    assert_eq!(result, 20);
+//}
+//
+//fn main() {
+//    let mut number = 3;
+//
+//    while number != 0 {
+//        println!("{}!", number);
+//
+//        number = number - 1;
+//    }
+//
+//    println!("LIFTOFF!!!");
+//}
+
+//fn main() {
+//    let a = [10, 20, 30, 40, 50];
+//
+//    for element in a.iter() {
+//        println!("the value is: {}", element);
+//    }
+//}
